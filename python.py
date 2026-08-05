d2d7ccb2aaa63132e4ae4ab7688351e72403dfa9
Muhammad-Fazeel384 <akmalqamar123@gmail.com>
2026-08-05 08:12:48 +0500

    Update python.py

--- python.py
+++ python.py
@@ -6,7 +6,23 @@ def sub(a, b):
   
 def mul(a, b):
   return a*b
+  
 
 print(add(5, 6))
 print(sub(5, 6))
 print(mul(5, 6))
+
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+
+
+def div(a, b):
+  return a*b
+  
+def power(a, b):
+  return a**b
+print(div(5, 6))
+print(power(5, 6))
